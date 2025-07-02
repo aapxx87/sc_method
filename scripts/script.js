b01fa798cd31649cb287d42f39b0fcac6ad96b3a
aapxx87 <57785764+aapxx87@users.noreply.github.com>
2025-07-02 11:20:06 +0300

    update

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,11 @@
+// Selectors
+
+const container_navbar = document.querySelector('.navbar-container')
+
+const btn_hide_navbar = document.querySelector('.hide-navbar-btn')
+const btn_show_navbar = document.querySelector('.show-navbar-btn')
+
+
 document.addEventListener("DOMContentLoaded", () => {
   fetch('fragments/semantic_model.html')
     .then(response => response.text())
@@ -8,3 +16,15 @@ document.addEventListener("DOMContentLoaded", () => {
       console.error("Ошибка загрузки semantic_model.html", err);
     });
 });
+
+
+
+btn_hide_navbar.addEventListener('click', function () {
+  container_navbar.classList.remove("visible")
+  container_navbar.classList.add("hidden")
+})
+
+btn_show_navbar.addEventListener('click', function () {
+  container_navbar.classList.remove("hidden")
+  container_navbar.classList.add("visible")
+})
